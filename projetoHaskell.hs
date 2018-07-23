51fd809b0d8f56feab2074d82b695c13b64f36af
marianaalucena <mariana.lucena@ccc.ufcg.edu.br>
2018-07-23 10:57:56 -0300

    Iniciando o menu

---
+++ projetoHaskell.hs
@@ -0,0 +1,14 @@
+-- função que exibe o Menu
+
+menu :: IO()
+menu = do
+		system "clear"
+		
+		putStrLn "Jogo da Memória"
+		putStrLn "\nMenu:"
+		putStrLn "\n1. Iniciar jogo"
+		putStrLn "\n2. Mostrar Ranking"
+		putStrLn "\n3. Sair"
+	
+		
+
