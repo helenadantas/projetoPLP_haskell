9ab8b9dd93d7bad5af592ab267b3adb563d0dfb1
TARSILA SOUZA DE ARRUDA SANTOS 115211087 <tarsilasas@lcc2-11.lcc.ufcg.edu.br>
2018-07-26 16:37:03 -0300

    Jogo da Memoria funcionando

--- projetoHaskell.hs
+++ projetoHaskell.hs
@@ -1,124 +1,72 @@
--- Funnção que exibe o Menu
+main = do putStrLn("Menu:" ++ "\n")
+          putStrLn "Jogo da Memória"
+          putStrLn "\nMenu:"
+          putStrLn "\nEscolha um nível:"
+          putStrLn "\n1. Iniciante"
+          putStrLn "\n2. Intermediario"
+          putStrLn "\n3. Avancado"
+          putStrLn "\n4. Sair\n"
+          
+          number <- readLn
+          menu number
 
-menu :: IO()
-menu = do
-		system "clear"
-		
-		putStrLn "Jogo da Memória"
-		putStrLn "\nMenu:"
-		putStrLn "\n1. Iniciar jogo"
-		putStrLn "\n. Sair"
-		
--- Escolha do nivel
-nivel :: String -> IO()
-nivel nome = do
-	system "clear"
-	putStrLn(" Escolha um dos niveis:\n\n" ++
-			"1. Iniciante\n" ++
-			"2. Intermediario\n" ++
-			"3. Avancado\n\n")
-	dados <- getLine
-	let niveis = read(dados)
-	if niveis == 1 then
-		inicializa niveis 4 nome
-	else if niveis == 2 then
-		inicializa niveis 6 nome
-	else if niveis == 3 then
-		inicializa niveis 8 nome
-	else
-		nivel nome
+menu :: Int -> IO()
+menu 1 = do
+       let matrizIniciante = [[4,6,7,2], [1,3,5,2], [4,5,6,3], [1,8,8,7]]
+       let matrizInicianteUsuario = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+       jogada 8 0 matrizIniciante matrizInicianteUsuario
+menu 2 = do 
+       let matrizIntermediaria = [[18,5,8,2,4,9], [5,16,12,7,3,10], [1,17,8,6,11,18], [10,3,17,14,4,13], [7,15,2,11,16,6], [12,14,9,1,15,13]]
+       let matrizIntermediariaUsuario = [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]]
+       jogada 18 0 matrizIntermediaria matrizIntermediariaUsuario
+menu 3 = do 
+       let matrizAvancada = [[10,4,9,2,14,14,21,26], [23,17,29,11,20,1,30,30], [4,27,17,5,24,29,6,6], [26,28,11,8,13,16,12,31], [21,22,2,19,19,1,25,25], [3,27,10,32,24,15,7,12], [23,18,28,5,16,15,31,13], [9,22,3,18,20,32,7,8]]          
+       let matrizAvancadaUsuario = [[0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]]
+       jogada 32 0 matrizAvancada matrizAvancadaUsuario
+menu 4 = do putStrLn("\n" ++ "Programa finalizado.")
 
--- FALTA CODAR O insereElementoMatriz
 
--- Modifica a matriz
-modificaMatriz :: Int -> String -> (Int,Int) -> [[String]] -> IO [[String]]
-modificaMatriz tamanho elem l c matriz = do
-	let linha1 = l
-	let coluna1 = c
-	let matrizMod = insereElementoMatriz tamanho linha1 coluna1 elem matriz [[]]
-	return matrizMod
 
--- Inicialização do jogo
-inicializa :: Int -> Int -> IO()
-inicializa nivel tamanho = do
-	let matrizDoUsuario = (matrizUsuario tamanho)
-	matrizSistema <- geraMatriz tamanho
-	jogo tamanho nivel 0 matrizDoUsuario matrizSistema
-	putStrLn "\n"
-	
--- Controller
-controller :: Int -> Int -> Int -> [[String]] -> [[Int]] -> IO()
-controller tamanho nivel paresEncontrados matrizUsuario matrizInt = do
-	let tamMatriz = tamanho * tamanho
-	let xD =  intToDouble x
-	let g = xD / 2
-	let pares = round g
+jogada ::Int -> Int -> [[Int]] -> [[Int]] -> IO()
+jogada 0 jogadas matrizI matrizF = putStrLn "Fim de Jogo" 
+jogada i j (s:cs) (x:xs) = do
+        putStrLn "\nDigite a linha do primeiro elemento:"
+        linha1 <- getLine
+        let l1 = (read linha1) :: Int
+        putStrLn "\nDigite a coluna do primeiro elemento:"
+        coluna1 <- getLine
+        let c1 = (read coluna1) :: Int
+        putStrLn "\nDigite a linha do segundo elemento:"
+        linha2 <- getLine
+        let l2 = (read linha2) :: Int
+        putStrLn "\nDigite a coluna do segundo elemento:"
+        coluna2 <- getLine
+        let c2 = (read coluna2) :: Int
+        
+        let e1 = getElemento (s:cs) l1 c1 
+        let e2 = getElemento (s:cs) l2 c2
+        let aux = modificaLinha ((x:xs)!!l1) e1 c1
+        let matrizusuario1 = atualizaMatrizUsuario (x:xs) l1 aux                   
+        let aux2 = modificaLinha (matrizusuario1!!l2) e2 c2
+        let matrizUsuarioAtualizada = atualizaMatrizUsuario matrizusuario1 l2 aux2
+        print matrizUsuarioAtualizada
+        
+        if (e1 == e2) then do jogada (i-1) (j+1) (s:cs) matrizUsuarioAtualizada else jogada i (j+1) (s:cs) (x:xs)
+               
+                           
+modificaLinha :: [Int] -> Int -> Int -> [Int]
+modificaLinha [] elemento coluna = []
+modificaLinha (h:hs) elemento 0 =  (elemento:hs)
+modificaLinha (h:hs) elemento coluna = (h:(modificaLinha hs elemento (coluna-1)))
 
-	if (paresEncontrados /= pares) then do
-		
-		matrizPrint nivel matrizInt
-		putStrLn "\nDigite a linha do primeiro elemento:"
-		l1 <- getLine
-		putStrLn "\nDigite a coluna do primeiro elemento:"
-		c1 <- getLine
-		putStrLn "\nDigite a linha do segundo elemento:"
-		l2 <- getLine
-		putStrLn "\nDigite a coluna do segundo elemento:"
-		c2 <- getLine
-		
-		
-		let linha1 = read(l1)
-		let coluna1 = read(c1)
-		let linha2 = read(l2)
-		let coluna2 = read(c2)
-		if (verificaPares linha1 coluna1 linha2 coluna2 matrizInt) then do
-			let posicao = (linha1, coluna1)
-			let elem = matrizInt !! (linha1 -1) !! (coluna1 -1)
-			
-			-- verificar a posicaoValida e modifica matriz
-			
-			
-			cond <- verificaPosicao posicao matrizInt
-			if cond then do
-				matrizM <- modificaMatriz tamanho elem posicao matrizUsuario
-				let posicao2 = (linha2, coluna2)
-				let elem2 = matrizInt !! (linha2 -1) !! (coluna -1)
-				matrizM2 <- modificaMatriz tamanho elem2 posicao2 matrizM
-				jogo tamanho nivel (paresEcontrados+1) matrizM2 matrizInt
-			else do
-				putStrLn("Posicao ja encontrada")
-				jogo tamanho nivel paresEcontrados matrizUsuario matrizInt
-		else do
-				let posicao = (linha1, coluna1)
-				let elem = matrizInt !! (linha1 -1) !! (coluna1 -1)
-				matrizM <- modificaMatriz tamanho elem posicao matrizUsuario
-				let posicao2 = (linha2, coluna2)
-				let elem2 = matrizInt !! (linha2 -1) !! (coluna2 -1)
-				matrizM2 <- modificaMatriz tamanho elem2 posicao2 matrizM
-				matrizPrint nivel matrizM2
-				jogo tamanho nivel paresEcontrados matrizUsuario matrizInt
-		else do
-			system "clear"
-			putStrLn "As posicoes sao invalidas"
-			jogo tamanho nivel paresEcontrados matrizUsuario matrizInt
-	
-	
-	-- Nao sei se ta certo
-	putStrLn "Fim de jogo"
+atualizaMatrizUsuario :: [[Int]] -> Int -> [Int] -> [[Int]]
+atualizaMatrizUsuario [] e1 _  = []
+atualizaMatrizUsuario (h:ht) 0 aux = aux:ht 
+atualizaMatrizUsuario (h:ht) l1 aux = h : (atualizaMatrizUsuario ht (l1-1) aux)
 
--- verificando posicao ~sem testar~	
-verificaPosicao :: Int -> Int -> [[String]] -> IO Bool
-verificaPosicao l c matriz = do
-	let linha = l - 1
-	let coluna = c - 1
-	if (matriz !! linha !! coluna == "*") then 
-		return True
-	else return False
-	
---verifica se os pares escolhidos pelo usuario são iguais na matriz
-verificaPares :: Int-> Int-> Int-> Int-> [[Int]] -> Int
-verificaPares coluna1 linha1 coluna2 linha2 
-	| matriz !! (linha1-1) !! (coluna1-1) == matriz !! (linha2-1) !! (coluna2-1) = true
-    | otherwise = false
-    
+                                                                         
+getElemento :: [[Int]] -> Int -> Int -> Int
+getElemento matriz linha coluna = matriz !! linha !! coluna
+
+
 
